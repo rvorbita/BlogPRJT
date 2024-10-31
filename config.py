921249f1cb3be46fd33e4a83b614dfafd815c222
Raymart Orbita <raymart.orbita@infor.com>
2024-10-31 20:59:50 +0800

    added web configuration

---
+++ config.py
@@ -0,0 +1,5 @@
+
+#form secret key
+SECRET_KEY='8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+#DB URI
+DB_URI='sqlite:///posts.db'
--- main.py
+++ main.py
@@ -12,6 +12,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 '''
@@ -28,7 +33,8 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -57,7 +63,9 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
